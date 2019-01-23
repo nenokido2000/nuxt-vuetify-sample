6134d1551c6d8737e08dc25259d88d3ccc6f2428
Naoki Enokido <naoki_enokido@NaokinoMacBook-puro.local>
2019-01-23 12:09:10 +0900

    update: loading表示を追加

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -33,6 +33,7 @@
           >
             <v-text-field
               v-model="status"
+              :counter="10"
               label="ステータス"
             />
           </v-flex>
@@ -43,10 +44,12 @@
       slot="activator"
       color="blue lighten-2"
       dark
+      @click="search"
     >
       検索
     </v-btn>
     <v-data-table
+      :loading="loading"
       :headers="headers"
       :items="orders"
       class="elevation-1"
@@ -183,6 +186,7 @@
 export default {
   data() {
     return {
+      loading: false,
       id: '',
       register_date: '',
       status: '',
@@ -193,42 +197,49 @@ export default {
         { text: 'ステータス', value: 'status' },
         { text: 'アクション', value: 'action' }
       ],
-      orders: [
-        {
-          id: 1,
-          register_date: '2018/12/30',
-          status: '処理中'
-        },
-        {
-          id: 2,
-          register_date: '2018/12/30',
-          status: '処理中'
-        },
-        {
-          id: 3,
-          register_date: '2018/12/30',
-          status: '処理中'
-        },
-        {
-          id: 4,
-          register_date: '2018/12/30',
-          status: '処理中'
-        },
-        {
-          id: 5,
-          register_date: '2018/12/30',
-          status: '処理中'
-        },
-        {
-          id: 6,
-          register_date: '2018/12/30',
-          status: '処理中'
-        }
-      ]
+      orders: []
     }
   },
   layout: 'contacts',
   methods: {
+    search() {
+      this.loading = true
+      setTimeout(() => {
+        this.orders = [
+          {
+            id: 1,
+            register_date: '2018/12/30',
+            status: '処理中'
+          },
+          {
+            id: 2,
+            register_date: '2018/12/30',
+            status: '処理中'
+          },
+          {
+            id: 3,
+            register_date: '2018/12/30',
+            status: '処理中'
+          },
+          {
+            id: 4,
+            register_date: '2018/12/30',
+            status: '処理中'
+          },
+          {
+            id: 5,
+            register_date: '2018/12/30',
+            status: '処理中'
+          },
+          {
+            id: 6,
+            register_date: '2018/12/30',
+            status: '処理中'
+          }
+        ]
+        this.loading = false
+      }, 3000)
+    },
     showItem(id) {
       alert(`show : ${id}`)
     },
--- plugins/i18n/vuetify/ja.js
+++ plugins/i18n/vuetify/ja.js
@@ -1,14 +1,14 @@
 export default {
   dataIterator: {
     rowsPerPageText: 'Items per page:',
-    rowsPerPageAll: 'All',
+    rowsPerPageAll: '全件',
     pageText: '{2} 件中 {0}-{1} 件を表示',
     noResultsText: 'No matching records found',
-    nextPage: 'Next page',
-    prevPage: 'Previous page'
+    nextPage: '次のページ',
+    prevPage: '前のページ'
   },
   dataTable: {
     rowsPerPageText: '表示数:'
   },
-  noDataText: 'No data available'
+  noDataText: '検索結果がありません'
 }
